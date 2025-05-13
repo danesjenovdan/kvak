7adf10051a954d00137c1ac95ce98fbcf687e390
Tomaž Kunst <tomazkunst@gmail.com>
2025-05-13 12:10:25 +0200

    initial project

---
+++ kvak/kvak/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-hz_13%qy^^2(e1!t#hloa3)$&6$=llxm_*)js7$tq57jg2=b(9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
