6edc6877d35eda962534fb964acce2977c2e408b
LepkoQQ <lepko.san@gmail.com>
2025-08-19 12:02:38 +0200

    add favicon

--- kvak/kvak/urls.py
+++ kvak/kvak/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
+from django.templatetags.static import static
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -8,6 +10,7 @@ from wagtail.documents import urls as wagtaildocs_urls
 from users.views import RegisterView
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=static("favicon/favicon.ico"))),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
