5aa2aa07414d285cd7297ad5a578ae69f6ff7056
LepkoQQ <lepko.san@gmail.com>
2025-08-20 15:39:04 +0200

    add header settings model

--- kvak/kvak/settings/base.py
+++ kvak/kvak/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "users",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
+    "wagtail.contrib.settings",
     "wagtail.embeds",
     "wagtail.sites",
     "kvak.apps.CustomUsersAppConfig",
@@ -85,6 +86,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "wagtail.contrib.settings.context_processors.settings",
             ],
             "loaders": [
                 (
