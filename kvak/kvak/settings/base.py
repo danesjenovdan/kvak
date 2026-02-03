d922b9fd439e2d9ed7f74f537d580559f62ed348
LepkoQQ <miha@lepko.net>
2026-02-03 14:30:42 +0100

    update requirements

--- kvak/kvak/settings/base.py
+++ kvak/kvak/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "django_components",
     "django_countries",
     "debug_toolbar",
